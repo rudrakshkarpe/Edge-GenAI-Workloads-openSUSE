cd8eee0bf268644747bee3e17b13aa042d429a70
rudrakshkarpe <rudraksh.karpe@gmail.com>
2024-08-09 03:52:55 +0530

    feat: create basic chatbot UI

---
+++ pages/chatbox.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from utils.ollama import chat, context_chat
+
+
+def chatbox():
+    if prompt := st.chat_input("What help do you need?:"):
+        if not st.session_state["query_engine"]:
+            st.warning("Please wait for the model to load.")
+            st.stop()
+            
+        st.session_state["messages"].append({"role": "user", "message": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+            
+            
+        with st.chat_message("assistant"):
+            with st.spinner("Processing...."):
+                response = st.write_system(
+            
+                    context_chat(
+                        prompt=prompt, query_engine=st.session_state["query_engine"]
+                    )
+                    
+                )
+        st.session_state["messages"].append({"role": "assistant", "message": response})
+        
+        
+        
+        
+        
+        
