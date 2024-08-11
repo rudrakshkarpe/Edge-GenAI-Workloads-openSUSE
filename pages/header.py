e66aa480b55885ce2d593649b36aa78d0689ce59
rudrakshkarpe <rudraksh.karpe@gmail.com>
2024-08-12 05:25:38 +0530

    feat: create gradient heading style

--- pages/header.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def set_page_header():
-    st.header("🏠 Localization of LLMs", anchor=False)
-    st.caption(
-        "A retrieval augmented generation (RAG) platform with open-source Large Language Models (LLMs) without exposing your data to online LLM providers."
-    )
