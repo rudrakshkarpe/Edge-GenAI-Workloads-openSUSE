f45f1b67175a694ae22d97fdbcec251e8b62d23c
rudrakshkarpe <rudraksh.karpe@gmail.com>
2024-08-12 23:21:46 +0530

    try: update color gradient

--- modules/header.py
+++ modules/header.py
@@ -1,13 +1,13 @@
 import streamlit as st
-
+# from streamlit_image_select import image_select
 
 def set_page_header():
     gradient_text_html = """
         <style>
         .gradient-text {
             font-weight: bold;
-            background: -webkit-linear-gradient(left, red, orange);
-            background: linear-gradient(to right, red, orange);
+            background: -webkit-linear-gradient(left, green, yellow, green);
+            background: linear-gradient(to right, green, orange, red);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
             display: inline;
@@ -16,9 +16,8 @@ def set_page_header():
         </style>
         <div class="gradient-text">LLMs Localization</div>
         """
-        
+
     st.markdown(gradient_text_html, unsafe_allow_html=True)
-    
 
     st.caption(
         "A retrieval augmented generation (RAG) platform with open-source Large Language Models (LLMs) without exposing your data to online LLM providers."
